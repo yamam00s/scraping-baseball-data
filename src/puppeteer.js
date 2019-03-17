6831093774dff704b22bd5b4626915277032cf12
yamam00s <s.yamamo0327@gmail.com>
2019-03-17 22:16:27 +0900

    配列に変換

--- src/puppeteer.js
+++ src/puppeteer.js
@@ -1,24 +1,27 @@
+/* eslint-disable no-console */
 const puppeteer = require('puppeteer');
 
 try {
   (async () => {
     const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
-
     await page.goto('https://baseball.yahoo.co.jp/npb/stats/batter?series=5&type=1', {
       waitUntil: 'domcontentloaded',
     });
 
     const listSelector = 'table.NpbPlSt tr:not(.yjMS)'; // .yjMSはtableの上下にある項目名のため除外
-    const dataList = await page.$$eval(listSelector, text => text.map(data => data.textContent));
+    const extractedDataList = await page.$$eval(listSelector, (element) => {
+      const handledDataList = element.map(data => data.textContent.split(/\n/));
+      return handledDataList;
+    });
 
-    await dataList.forEach((data) => {
-      const result = data.split(/\n/);
-      console.log(`data: ${result}`);
+    extractedDataList.forEach((data) => {
+      console.log(data);
     });
 
     browser.close();
   })();
 } catch (err) {
+  // eslint-disable-next-line no-console
   console.error(err);
 }
