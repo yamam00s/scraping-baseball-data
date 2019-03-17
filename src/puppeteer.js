5ff0076d76045339538e5477bdc1cccfe3270df0
yamam00s <s.yamamo0327@gmail.com>
2019-03-17 14:42:56 +0900

    DOMからデータ抽出

--- src/puppeteer.js
+++ src/puppeteer.js
@@ -1,5 +1,24 @@
-const pupppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');
 
-(async() => {
+try {
+  (async () => {
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const page = await browser.newPage();
 
-})();
+    await page.goto('https://baseball.yahoo.co.jp/npb/stats/batter?series=5&type=1', {
+      waitUntil: 'domcontentloaded',
+    });
+
+    const listSelector = '.NpbPlSt tr:not(.yjMS)';
+    const dataList = await page.$$eval(listSelector, text => text.map(data => data.textContent));
+
+    await dataList.forEach((data) => {
+      const result = data.split(/\n/);
+      console.log(`data: ${result}`);
+    });
+
+    browser.close();
+  })();
+} catch (err) {
+  console.error(err);
+}
