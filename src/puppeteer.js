6d4cb25d39ae06a0b561a1e9c3fca04aeb5c7abd
yamam00s <s.yamamo0327@gmail.com>
2019-03-23 18:48:45 +0900

    update eslintrc

--- src/puppeteer.js
+++ src/puppeteer.js
@@ -1,20 +1,27 @@
 /* eslint-disable no-console */
-const puppeteer = require('puppeteer');
-const BaseballJsonList = require('./modules/BaseballJsonList');
+const puppeteer = require("puppeteer");
+const BaseballJsonList = require("./modules/BaseballJsonList");
 
 try {
   (async () => {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-    const page = await browser.newPage();
-    await page.goto('https://baseball.yahoo.co.jp/npb/stats/batter?series=5&type=1', {
-      waitUntil: 'domcontentloaded',
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
     });
+    const page = await browser.newPage();
+    await page.goto(
+      "https://baseball.yahoo.co.jp/npb/stats/batter?series=5&type=1",
+      {
+        waitUntil: "domcontentloaded"
+      }
+    );
 
-    const listSelector = 'table.NpbPlSt tr:not(.yjMS)'; // .yjMSはtableの上下にある項目名のため除外
-    const extractedDataList = await page.$$eval(listSelector, (element) => {
+    const listSelector = "table.NpbPlSt tr:not(.yjMS)"; // .yjMSはtableの上下にある項目名のため除外
+    const extractedDataList = await page.$$eval(listSelector, element => {
       const handledDataList = element.reduce((prev, current) => {
         // 多重配列作成時にblankなデータは取り除く
-        const currentTextList = current.textContent.split(/\n/).filter(data => data !== '');
+        const currentTextList = current.textContent
+          .split(/\n/)
+          .filter(data => data !== "");
         prev.push(currentTextList);
         return prev;
       }, []);
