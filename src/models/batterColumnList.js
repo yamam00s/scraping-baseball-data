77edc1e3701c9a279d4e75b5fd57991261a05b31
yamam00s <s.yamamo0327@gmail.com>
2019-04-07 14:35:36 +0900

    keyを英語表記に

--- src/models/batterColumnList.js
+++ src/models/batterColumnList.js
@@ -1,29 +1,29 @@
 module.exports = [
-  '順位',
-  '名前',
-  'チーム',
-  '打率',
-  '試合',
-  '打席',
-  '打数',
-  '安打',
-  '二塁打',
-  '三塁打',
-  '本塁打',
-  '塁打',
-  '打点',
-  '得点',
-  '三振',
-  '四球',
-  '死球',
-  '犠打',
-  '犠飛',
-  '盗塁',
-  '盗塁死',
-  '併殺打',
-  '出塁率',
-  '長打率',
-  'OPS',
-  '得点圏',
-  '失策'
+  'RANK', // 順位
+  'NAME', // 名前
+  'TEAM', // 所属チーム
+  'AVG', // BATTING AVERAGE 打率
+  'GAMES', // GAMES PLAYED 試合数
+  'PA', // PLATE APPEARANCES 打席数
+  'AB', // AT BATS 打数
+  'H', // HITS 安打数
+  '2B', // 二塁打数
+  '3B', // 三塁打数
+  'HR', // 本塁打数
+  'TB', // TOTAL BASES 塁打数
+  'RBI', // RUNS BATTED IN 打点数
+  'RS', // RUN SCORED 得点数
+  'SO', // STRIKEOUTS 三振数
+  'BB', // BASES ON BALLS 四球数
+  'HBP', // HIT BY PITCH 死球
+  'SH', // SACRIFICE HITS 犠打数
+  'SF', // SACRIFICE FLIES 犠飛数
+  'SB', // STOLEN BASES 盗塁数
+  'CS', // CAUGHT STEALLING 盗塁死数
+  'GDP', // GROUND INTO DOUBLE PLAYS 併殺打数
+  'OBP', // ON BASE PERCENTAGE 出塁率
+  'SLG', // SLUGGING PERCENTAGE 長打率
+  'OPS', // ON BASE PLUS SLUGGING PERCENTAGE
+  'RISP', // RUNNER IN SCORING POSITION 得点圏
+  'E' // ERRORS 失策数
 ];
