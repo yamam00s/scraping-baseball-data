37599ccbcc17ad5aab1b30e5329c7f1e70adf619
yamam00s <s.yamamo0327@gmail.com>
2019-05-19 16:17:02 +0900

    add GET時のパラメータ取得と400処理

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,22 @@
 const functions = require('firebase-functions');
 const scrapingApp = require('./app.bundle');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+exports.scrapingBaseball = functions.https.onRequest(async (req, res) => {
+  // nuxtのlocal環境へのCORS設定
+  res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
 
-exports.scrapingBaseball = functions.https.onRequest(
-  async (request, response) => {
-    const scrapingData = await scrapingApp;
-    // nuxtのlocal環境へのCORS設定
-    response.set('Access-Control-Allow-Origin', 'http://localhost:3000');
-    response.send(scrapingData);
+  if (req.body.league === 'central' || req.body.league === 'pacific') {
+    res.status(400).send('Please define a league! central or pacific');
   }
-);
+  if (req.body.type === 'batter' || req.body.type === 'pitcher') {
+    res.status(400).send('Please define a type! batter or pitcher');
+  }
+
+  const scrapingData = await scrapingApp({
+    league: req.body.league,
+    type: req.body.type
+  });
+  res.status(200).send(scrapingData);
+});
